6e8947a9986c01534ffc8555944abdf6cedce5ff
tuomasme <mertanentuomas@gmail.com>
2024-11-09 14:57:59 +0200

    Initial commit

---
+++ server/src/main/java/com/apps/calorie_counter/CalorieCounterApplication.java
@@ -0,0 +1,13 @@
+package com.apps.calorie_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieCounterApplication.class, args);
+	}
+
+}
