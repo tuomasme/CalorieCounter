8324da75cdb2ccb885d1bfa8d03772a11bd5bdb5
tuomasme <mertanentuomas@gmail.com>
2025-03-30 15:36:28 +0300

    Improve meal form

--- client/src/components/IngredientFields.jsx
+++ client/src/components/IngredientFields.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { useFieldArray, useFormContext } from "react-hook-form";
 
 const IngredientFields = ({ fieldIndex }) => {
+  const [ingredientCount, setIngredientCount] = useState(1);
   const { register } = useFormContext();
   const {
     fields: ingredients,
@@ -13,28 +15,77 @@ const IngredientFields = ({ fieldIndex }) => {
   return (
     <fieldset>
       <div className="flex justify-center">
-        <legend>Ingredients</legend>
+        <legend>Victual Ingredients</legend>
       </div>
       {ingredients.map((ingredient, index) => (
-        <section key={ingredient.id} className="flex justify-center gap-4">
-          <input
-            className="input input-bordered"
-            {...register(`victuals.${fieldIndex}.ingredients.${index}.name`)}
-          />
-          <button
-            type="button"
-            className="btn btn-secondary"
-            onClick={() => removeIngredient(index)}
-          >
-            Remove
-          </button>
+        <section key={ingredient.id} className="mb-4">
+          <div className="flex justify-center">
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Ingredient Name</span>
+              </div>
+              <input
+                className="input input-bordered"
+                {...register(
+                  `victuals.${fieldIndex}.ingredients.${index}.name`
+                )}
+              />
+            </label>
+          </div>
+          <div className="flex justify-center gap-4">
+            <label className="form-control w-1/3 max-w-xs">
+              <div className="label">
+                <span className="label-text">Ingredient Weight</span>
+              </div>
+              <input
+                type="number"
+                min="0.0"
+                step="1"
+                className="input input-bordered"
+                {...register(
+                  `victuals.${fieldIndex}.ingredients.${index}.weight`
+                )}
+              />
+            </label>
+            <label className="form-control w-1/3 max-w-xs">
+              <div className="label">
+                <span className="label-text">Ingredient Calories</span>
+              </div>
+              <input
+                type="number"
+                min="0.0"
+                step=".1"
+                className="input input-bordered"
+                {...register(
+                  `victuals.${fieldIndex}.ingredients.${index}.calories`
+                )}
+              />
+            </label>
+          </div>
+          {ingredientCount > 1 ? (
+            <div className="flex justify-center">
+              <button
+                type="button"
+                className="btn btn-secondary w-full max-w-xs mt-2"
+                onClick={() => {
+                  removeIngredient(index);
+                  setIngredientCount(ingredientCount - 1);
+                }}
+              >
+                Remove
+              </button>
+            </div>
+          ) : null}
         </section>
       ))}
-      <div className="flex justify-center gap-4">
+      <div className="flex justify-center">
         <button
           type="button"
-          className="btn btn-success"
-          onClick={() => appendIngredient({ name: "" })}
+          className="btn btn-info w-full max-w-xs"
+          onClick={() => {
+            appendIngredient({ name: "", weight: 0, calories: 0.0 });
+            setIngredientCount(ingredientCount + 1);
+          }}
         >
           Append Ingredient
         </button>
--- client/src/components/MealForm.jsx
+++ client/src/components/MealForm.jsx
@@ -1,11 +1,15 @@
 import { useForm, useFieldArray, FormProvider } from "react-hook-form";
 import IngredientFields from "./IngredientFields";
+import { useState } from "react";
 
 const defaultValues = {
-  victuals: [{ name: "", ingredients: [{ name: "" }] }],
+  victuals: [
+    { name: "", ingredients: [{ name: "", weight: 0, calories: 0.0 }] },
+  ],
 };
 
 const MealForm = ({ isOpen, onClose, mode, OnSubmit }) => {
+  const [victualCount, setVictualCount] = useState(1);
   const form = useForm({
     defaultValues,
   });
@@ -60,33 +64,68 @@ const MealForm = ({ isOpen, onClose, mode, OnSubmit }) => {
                 {fields.map((field, index) => (
                   <fieldset key={field.id}>
                     <div className="flex justify-center">
-                      <legend>Victual {index + 1}</legend>
+                      <legend>Meal Victual</legend>
                     </div>
                     <div className="flex mb-4 justify-center">
-                      <input
-                        className="input input-bordered"
-                        {...register(`victuals.${index}.name`)}
-                      />
+                      <label className="form-control w-full max-w-xs">
+                        <div className="label">
+                          <span className="label-text">Victual Name</span>
+                        </div>
+                        <input
+                          className="input input-bordered"
+                          {...register(`victuals.${index}.name`)}
+                        />
+                      </label>
                     </div>
                     <IngredientFields fieldIndex={index} />
-                    <section className="mt-4 mb-6 flex justify-center gap-4">
-                      <button
-                        type="button"
-                        className="btn btn-success"
-                        onClick={() =>
-                          appendVictual({ name: "", ingredients: [] })
-                        }
-                      >
-                        Append Victual
-                      </button>
-                      <button
-                        type="button"
-                        className="btn btn-secondary"
-                        onClick={() => removeVictual(index)}
-                      >
-                        Remove
-                      </button>
-                    </section>
+                    {victualCount > 1 ? (
+                      <section className="mt-4 mb-6 flex justify-center gap-4">
+                        <button
+                          type="button"
+                          className="btn btn-info w-1/3 max-w-xs"
+                          onClick={() => {
+                            appendVictual({
+                              name: "",
+                              ingredients: [
+                                { name: "", weight: 0, calories: 0.0 },
+                              ],
+                            });
+                            setVictualCount(victualCount + 1);
+                          }}
+                        >
+                          Append Victual
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-secondary w-1/3 max-w-xs"
+                          onClick={() => {
+                            console.log(index);
+                            removeVictual(index);
+                            setVictualCount(victualCount - 1);
+                          }}
+                        >
+                          Remove
+                        </button>
+                      </section>
+                    ) : (
+                      <section className="mt-4 mb-6 flex justify-center gap-4">
+                        <button
+                          type="button"
+                          className="btn btn-info w-full max-w-xs"
+                          onClick={() => {
+                            appendVictual({
+                              name: "",
+                              ingredients: [
+                                { name: "", weight: 0, calories: 0.0 },
+                              ],
+                            });
+                            setVictualCount(victualCount + 1);
+                          }}
+                        >
+                          Append Victual
+                        </button>
+                      </section>
+                    )}
                   </fieldset>
                 ))}
               </div>
