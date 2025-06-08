3b9472c7ccc999433cff2354db7cd7e6aad1d99f
tuomasme <mertanentuomas@gmail.com>
2025-06-08 11:08:21 +0300

    Update meal

--- server/src/main/java/com/apps/calorie_counter/meal/MealRepository.java
+++ server/src/main/java/com/apps/calorie_counter/meal/MealRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public interface MealRepository extends JpaRepository<Meal, Long> {
 
-    @Query(value = "SELECT m.id as mealId, m.time as mealTime, SUM(i.calories) as mealCalories FROM meals m JOIN victuals v ON m.id = v.meal_id JOIN ingredients i ON v.id = i.victual_id GROUP BY m.id", nativeQuery = true)
+    @Query(value = "SELECT m.id as id, m.time as time, COALESCE(SUM(i.calories),0) as mealCalories FROM meals m JOIN victuals v ON m.id = v.meal_id JOIN ingredients i ON v.id = i.victual_id GROUP BY m.id", nativeQuery = true)
     List<IMealTotalCalories> getMealTotalCalories();
 
     @Query(value = "SELECT m.time as mealTime, v.id as victualId, v.name as victualName, i.id as ingredientId, i.name as ingredientName, i.weight as ingredientWeight, i.calories as ingredientCalories FROM ingredients i JOIN victuals v ON i.victual_id = v.id JOIN meals m ON v.meal_id = m.id WHERE m.id = ?1", nativeQuery = true)
